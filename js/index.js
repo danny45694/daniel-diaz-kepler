ccfff365069abdf894816b2e886aab2b19040df9
Daniel Desktop <danieladiazop@gmail.com>
2025-05-04 21:49:46 -0700

    Worked more through the lesson and fixed some bugs

--- js/index.js
+++ js/index.js
@@ -1,26 +1,26 @@
+
+
 const body = document.body;
 const footerElement = document.createElement("footer");
-body.appendChild("footerElement");
+body.appendChild(footerElement);
 
 let today = new Date();
-
 let thisYear = today.getFullYear();
-
 const footer = document.querySelector("footer");
 
 let copyright = document.createElement("p");
+copyright.innerHTML = `Daniel Diaz ${thisYear} &copy`;
+footer.appendChild(copyright)
+body.appendChild(footer);
 
-copyright.innerHTML = `Daniel Diaz ${thisYear} '&copy'`;
-footer.appendChild("copyright")
 
-body.appendChild("footer");
+let skills = ["Javascript", "HTML", "CSS", "Adobe Photoshop", "GitHub"]
+let skillsSection = document.getElementById("skills");
+let skillsList = skillsSection.querySelector("ul");
 
-let skills = ["Javascript", "HTML", "CSS", "GitHub"]
-let skillsSection = document.querySelector("skills");
-let skillsList = document.querySelector("skillsSection");
-
-for(i = 0; i < skills.length; i++) {
+for (let i = 0; i < 2; i++) {
     let skill = document.createElement("li");
-    skill.innerText = skills[i];
-    skillsList.appendChild("skill");
+    skill.innerText = "bob";
+    skillsList.appendChild(skill);
+    console.log(i)
 }
