f2cb6d7c1a527b02336f7ce0404fdea36a2b2313
Daniel Desktop <danieladiazop@gmail.com>
2025-05-05 22:17:50 -0700

    Finally fixed the ul throwing null error bug

--- js/index.js
+++ js/index.js
@@ -1,26 +1,30 @@
 
 
-const body = document.body;
-const footerElement = document.createElement("footer");
-body.appendChild(footerElement);
+document.addEventListener('DOMContentLoaded', function() {
+    let body = document.body;
+    let footerElement = document.createElement("footer");
+    body.appendChild(footerElement);
 
-let today = new Date();
-let thisYear = today.getFullYear();
-const footer = document.querySelector("footer");
+    let today = new Date();
+    let thisYear = today.getFullYear();
+    let footer = document.querySelector("footer");
 
-let copyright = document.createElement("p");
-copyright.innerHTML = `Daniel Diaz ${thisYear} &copy`;
-footer.appendChild(copyright)
-body.appendChild(footer);
+    let copyright = document.createElement("p");
+    copyright.innerHTML = `Daniel Diaz ${thisYear} &copy`;
+    footer.appendChild(copyright)
+    body.appendChild(footer);
 
 
-let skills = ["Javascript", "HTML", "CSS", "Adobe Photoshop", "GitHub"]
-let skillsSection = document.getElementById("skills");
-let skillsList = skillsSection.querySelector("ul");
+    let skills = ["Javascript", "HTML", "CSS", "Adobe Photoshop", "GitHub"]
+    let skillsSection = document.getElementById("skills");
+    let skillsList = skillsSection.querySelector("ul");
 
-for (let i = 0; i < 2; i++) {
+    for (let i = 0; i < skills.length; i++) {
     let skill = document.createElement("li");
-    skill.innerText = "bob";
+    skill.innerText = skills[i];
     skillsList.appendChild(skill);
-    console.log(i)
-}
+    }
+
+  });
+
+
